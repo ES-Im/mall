5cfae3598b80afb6c8aeec8d008282a20b588fb8
eunseokim <eunseokim43@naver.com>
2024-11-26 14:11:17 +0900

    은서 ) Cart - addCart, removeCart 구현, customer.login() service, mapper 구현

--- src/main/java/com/example/mall/controller/CartController.java
+++ src/main/java/com/example/mall/controller/CartController.java
@@ -5,9 +5,11 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.mall.service.CartService;
+import com.example.mall.vo.Cart;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
@@ -16,12 +18,22 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CartController {
 	@Autowired CartService cartService;
-	@GetMapping("/customer/getCartListByCustomerId")
-	public String getCartListByCustomerId(HttpSession session) {
+	
+	// customer/getCartListByCustomerId 에서 장바구니 리스트 출력시 사용
+	@GetMapping("/customer/getCartList")
+	public String getCartList(Model model, HttpSession session) {
 		String customerEmail = (String) session.getAttribute("loginStaff");
-		List<Map<String, Object>> cartList = cartService.getCartListByCustomerId(customerEmail);
+		List<Map<String, Object>> cartList = cartService.getCartList(customerEmail);
+		model.addAttribute("cartList", cartList);
+		
 		return "customer/getCartListByCustomerId";
 	}
 	
-
+	// customer/addCart : cart에 상품 추가시 사용
+	@GetMapping("/customer/addCart")
+	public String addCart(Cart cart) {
+		cartService.addCart(cart);
+		return "redirect :/customer/getCartList";
+	}
+	
 }
--- src/main/java/com/example/mall/service/CustomerService.java
+++ src/main/java/com/example/mall/service/CustomerService.java
@@ -1,10 +1,22 @@
 package com.example.mall.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.example.mall.mapper.CustomerMapper;
+import com.example.mall.vo.Customer;
 
 @Service
 @Transactional
 public class CustomerService {
-
+	@Autowired CustomerMapper customerMapper;
+	
+	public String login(String id, String pw) {
+		Customer customer = new Customer();
+		customer.setCustomerEmail(id);
+		customer.setCustomerPw(pw);
+		
+		return customerMapper.login(customer);
+	}
 }
