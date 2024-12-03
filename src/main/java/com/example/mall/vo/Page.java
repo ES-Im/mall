b3d83a614b8a3db99df08c36412ab68238d26280
eunseokim <eunseokim43@naver.com>
2024-12-03 11:14:47 +0900

    은서 ) Page vo, view - Pagination 부분 수정

--- src/main/java/com/example/mall/vo/Page.java
+++ src/main/java/com/example/mall/vo/Page.java
@@ -11,16 +11,34 @@ public class Page {
     private Integer lastPage;
     
     // beginRow, StartPaging, EndPage는 get으로만 호출 가능
+    // 쿼리 Limit 설정시 사용하는 메서드
     public Integer getBeginRow() {
         return this.currentPage == null? 1 : (this.currentPage - 1) * this.rowPerPage;
     }
-
+    
+    // 현재 페이지가 속한 페이징 그룹의 첫 번째 번호를 계산
     public Integer getStartPagingNum() {
-        return this.currentPage == null? 1 : (this.currentPage - 1) / 10 * 10 + 1;
+        return this.currentPage == null? 1 : (this.currentPage - 1) / this.numPerPage * this.numPerPage + 1;
     }
-
+    
+    // 페이징 그룹의 마지막 번호를 계산하며 lastPage를 초과하지 않음
     public Integer getEndPagingNum() {
         return Math.min((this.getStartPagingNum() + (this.numPerPage - 1)), this.lastPage);
     }
-//
+    
+    // 이전 그룹의 마지막 번호를 계산, -1을 반환시 이동 불가
+    public Integer getPreviousGroupEnd() {
+    	return (this.currentPage - 1) - ((this.currentPage - 1) % this.numPerPage);
+    }
+    
+    // 다음 그룹의 첫 번째 번호를 반환, -1을 반환시 이동 불가 (마지막 그룹을 초과하지 않음)
+    public Integer getNextGroupStart() {
+    	return (this.getLastGroupStart() > this.currentPage)? (this.currentPage - 1) - ((this.currentPage - 1) % this.numPerPage) + this.numPerPage + 1 : 0;
+    }
+    
+    // 마지막 그룹의 첫 번째 번호를 계산
+    public Integer getLastGroupStart() {
+    	int index = this.lastPage - ((this.lastPage) % this.numPerPage) + 1;
+    	return (index > lastPage) ? index - this.numPerPage : index;  
+    }
 }
