b26e9247583a1929b08c1135ac38b9f40313d8dc
KDH <kimdongh7277@naver.com>
2024-12-05 11:09:22 +0900

    동현) selectGenderRatio 남여성비 조회

--- src/main/java/com/example/mall/service/StatisticalService.java
+++ src/main/java/com/example/mall/service/StatisticalService.java
@@ -1,5 +1,6 @@
 package com.example.mall.service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -19,8 +20,15 @@ public class StatisticalService {
 	@Autowired StatisticalMapper statisticalMapper;
 	
 	public Map<String, Object> getStatistical() {
-	    List<Map<String, Object>> resultDailySales = statisticalMapper.getDailySales();
-	    return resultDailySales.isEmpty() ? null : resultDailySales.get(0);  // 첫 번째 행 반환
+		
+	    List<Map<String, Object>> resultDailySales = statisticalMapper.selectDailySales(); // 하루 매출
+	    List<Map<String, Object>> resultGenderRatio = statisticalMapper.selectGenderRatio(); // 남여 성비
+	    
+	    Map<String, Object> resultMap = new HashMap<>();
+	    resultMap.put("getDailySales", resultDailySales.get(0));
+	    resultMap.put("getGenderRatio", resultGenderRatio);
+	    
+	    return resultMap;  // 첫 번째 행 반환
 	}
 
 }
